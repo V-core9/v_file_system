249fe7780eefe283f6688ee2f3cceeec4bfe63e8
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-09-04 01:13:19 +0200

    reorg file

--- tests/v_mkdir.test.js
+++ tests/v_mkdir.test.js
@@ -4,34 +4,35 @@
   
 // Include fs and path module
 const vMkDir = require('../src/v_mkdir');
+const vIsDir = require ('../src/v_is_dir');
 
-var dirsList = [ 'tests/testsZZ', 'tests/test/bin/js/co' ]
+var errorList = [];
+
+var dirsList = [ 
+  'tests/testsZZ', 
+  'tests/test/bin/js/co' 
+];
 
 setTimeout(() => {
   vMkDir(dirsList[0]);
+
+  setTimeout(() => {
+    vMkDir(dirsList[1], true);
+
+    setTimeout(() => {
+
+
+      var help1 = vIsDir(dirsList[0]);
+      var help2 = vIsDir(dirsList[1]);
+
+      console.log("DIR_1 Found: " + help1);
+      console.log("DIR_2 Found: " + help2);
+
+      if ( help1 && help2 ) {
+        console.log('\nTEST_MSG :_: All Directories Found.');
+        console.log('vMkDir OK -><[_ No Errors _]>');
+      }
+
+    }, 1000);
+  }, 300 );
 }, 150 );
-
-setTimeout(() => {
-  vMkDir(dirsList[1], true);
-}, 300 );
-
-
-const vIsDir = require ('../src/v_is_dir');
-
-setTimeout(() => {
-
-  var errorList = [];
-
-  var help1 = vIsDir(dirsList[0]);
-  var help2 = vIsDir(dirsList[1]);
-
-  console.log("DIR_1 Found: " + help1);
-  console.log("DIR_2 Found: " + help2);
-
-  if ( help1 && help2 ) {
-    console.log('\nTEST_MSG :_: All Directories Found.');
-    console.log('vMkDir OK -><[_ No Errors _]>');
-  }
-
-
-}, 1000);
