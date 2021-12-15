ca49dc6497534d06dfe2a37253846ba337e21023
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-12-15 14:55:50 +0100

    yea more tests for v_fs.promise ....some polishing for easier comebacks to it

--- __tests__/v_fs.promise.[method].test.js
+++ __tests__/v_fs.promise.[method].test.js
@@ -7,7 +7,13 @@ const demoFile = path.join(demoDir, 'sample_file.doc');
 const demoFile2 = path.join(demoDir, 'sample_file.txt');
 const testArray = ["sample_file.doc", "sample_file.txt"];
 
+const copyFile1 = path.join(demoDir, 'copy1_fileMETHOD.doc');
+const copyFile2 = path.join(demoDir, 'copy2_fileMETHOD.doc');
 
+
+
+
+//? mkdir  - - - - - - - - - - - - -
 
 
 test('mkdir Test 1', async () => {
@@ -22,7 +28,10 @@ test('mkdir Test 3', async () => {
   expect(await v_fs.promise.mkdir()).toEqual(false);
 });
 
+//! EOF - mkdir
 
+
+//? write  - - - - - - - - - - - - -
 
 test('write Test 1', async () => {
   expect(await v_fs.promise.write(demoFile, demoText)).toEqual(true);
@@ -40,7 +49,10 @@ test('write Test 4', async () => {
   expect(await v_fs.promise.write()).toEqual(false);
 });
 
+//! EOF - write
 
+
+//? read  - - - - - - - - - - - - -
 
 test('readPromise Test 1', async () => {
   expect(await v_fs.promise.read(demoFile)).toEqual(demoText);
@@ -54,7 +66,10 @@ test('readPromise Test 3', async () => {
   expect(await v_fs.promise.read()).toEqual(false);
 });
 
+//! EOF - read
 
+
+//? statsDir  - - - - - - - - - - - - -
 
 test('dirStatsRes Test 1', async () => {
   expect(await v_fs.promise.statsDir(demoFile)).toEqual(false);
@@ -70,7 +85,10 @@ test('dirStatsRes  Test 1', async () => {
   expect(await v_fs.promise.statsDir()).toEqual(false);
 });
 
+//! EOF - statsDir
 
+
+//? statsFile  - - - - - - - - - - - - -
 
 test('statsFile Test 1', async () => {
   expect(await v_fs.promise.statsFile(demoDir)).toEqual(false);
@@ -86,7 +104,10 @@ test('statsFile Test 3', async () => {
   expect(await v_fs.promise.statsFile()).toEqual(false);
 });
 
+//! EOF - statsFile
 
+
+//? isDir  - - - - - - - - - - - - -
 
 test('isDir Test 1', async () => {
   expect(await v_fs.promise.isDir(demoFile)).toEqual(false);
@@ -100,7 +121,10 @@ test('isDir Test 3', async () => {
   expect(await v_fs.promise.isDir()).toEqual(false);
 });
 
+//! EOF - isDir
 
+
+//? isFile  - - - - - - - - - - - - -
 
 test('isFilePromise Test 1', async () => {
   expect(await v_fs.promise.isFile(demoFile)).toEqual(true);
@@ -114,40 +138,84 @@ test('isFilePromise Test 3', async () => {
   expect(await v_fs.promise.isFile()).toEqual(false);
 });
 
+//! EOF - isFile
 
 
-test('listDirPromise Test 1', async () => {
+//? copy  - - - - - - - - - - - - -
+
+test('Copy Test 1', async () => {
+  expect(await v_fs.promise.copy()).toEqual(false);
+});
+
+test('copy Test 2', async () => {
+  expect(await v_fs.promise.copy(demoFile)).toEqual(false);
+});
+
+test('copy Test 3', async () => {
+  expect(await v_fs.promise.copy(demoFile, copyFile1)).toEqual(true);
+});
+
+test('copy Test 4', async () => {
+  expect(await v_fs.promise.copy(copyFile1, copyFile2)).toEqual(true);
+});
+
+//? Deleting those 2 copies
+test('deleteFile Test 001', async () => {
+  expect(await v_fs.promise.deleteFile(copyFile1)).toEqual(true);
+});
+
+test('deleteFile Test 002', async () => {
+  expect(await v_fs.promise.deleteFile(copyFile2)).toEqual(true);
+});
+
+//! EOF - copy
+
+
+
+//? listDir   - - - - - - - - - - - - -
+
+test('listDir Promise Test 1', async () => {
   expect(await v_fs.promise.listDir(demoDir)).toEqual(testArray);
 });
 
-test('listDirPromise Test 2', async () => {
+test('listDir Promise Test 2', async () => {
   expect(await v_fs.promise.listDir()).toEqual(false);
 });
 
+//! EOF - listDir
 
 
-test('deleteFileTest Test 1', async () => {
+
+//? deleteFile   - - - - - - - - - - - - -
+
+test('deleteFile Test 1', async () => {
   expect(await v_fs.promise.deleteFile(demoFile)).toEqual(true);
 });
 
-test('deleteFileTest Test 2', async () => {
+test('deleteFile Test 2', async () => {
   expect(await v_fs.promise.deleteFile(demoDir)).toEqual(false);
 });
 
-test('deleteFileTest Test 3', async () => {
+test('deleteFile Test 3', async () => {
   expect(await v_fs.promise.deleteFile()).toEqual(false);
 });
 
+//! EOF - deleteFile
 
 
-test('removeDirTest Test 1', async () => {
+
+//? removeDir   - - - - - - - - - - - - -
+
+test('removeDir Test 1', async () => {
   expect(await v_fs.promise.removeDir(demoFile)).toEqual(false);
 });
 
-test('removeDirTest Test 2', async () => {
+test('removeDir Test 2', async () => {
   expect(await v_fs.promise.removeDir(demoDir)).toEqual(true);
 });
 
-test('removeDirTest Test 3', async () => {
+test('removeDir Test 3', async () => {
   expect(await v_fs.promise.removeDir()).toEqual(false);
 });
+
+//! EOF - removeDir
